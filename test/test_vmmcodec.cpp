2472da243b4b456badaa133fea4d155001f2618c
Cenk Yildiz <cenk.yildiz@cern.ch>
2017-11-15 11:59:44 +0100

    Added vmmcodec test file, no real tests implemented

---
+++ test/test_vmmcodec.cpp
@@ -0,0 +1,18 @@
+/// Test suite for testing VMMCodec class
+
+#include <iostream>
+
+#define BOOST_TEST_MODULE my test module
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+void check_something() {
+    int a = 15;
+    int b = 15;
+    BOOST_TEST(a == b);
+}
+
+BOOST_AUTO_TEST_CASE(dummy_test) {
+    check_something();
+}
+
