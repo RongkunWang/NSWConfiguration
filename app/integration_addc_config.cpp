9aa36ffe0c631eb1d6b5e99b05697f6b7d41c5e6
Johanna Gramling <jgramlin@lxplus064.cern.ch>
2018-06-22 15:30:34 +0200

    modifications during testing with addc, works for now

--- app/integration_addc_config.cpp
+++ app/integration_addc_config.cpp
@@ -14,7 +14,7 @@ int main(int argc, const char *argv[])
 {
 
     // not sure if/what is needed for ADDC
-    std::string base_folder = "/afs/cern.ch/user/c/cyildiz/public/nsw-work/work2/NSWConfiguration/data/";
+    std::string base_folder = "/afs/cern.ch/user/c/cyildiz/public/nsw-work/work/NSWConfiguration/data/";
     nsw::ConfigReader reader1("json://" + base_folder + "integration_config.json"); //here is OPC address and SCA address that can be useful
     auto config1 = reader1.readConfig();
     write_json(std::cout, config1);
@@ -35,14 +35,27 @@ int main(int argc, const char *argv[])
     std::string sca_address = "SCA on ADDCv3";
     // std::string sca_roc_address_digital = sca_address + ".mmfe8RocCoreDigital";
 
+    //0: try to set them to 1 before everything
+    std::cout << "step 0" << std::endl;
+    cs.sendGPIO(opc_ip, sca_address + ".gpio.art0SRstn", 1);
+    cs.sendGPIO(opc_ip, sca_address + ".gpio.art1SRstn", 1);
+    cs.sendGPIO(opc_ip, sca_address + ".gpio.art0CRstn", 1);
+    cs.sendGPIO(opc_ip, sca_address + ".gpio.art1CRstn", 1);
+    cs.sendGPIO(opc_ip, sca_address + ".gpio.art0Rstn", 1);
+    cs.sendGPIO(opc_ip, sca_address + ".gpio.art1Rstn", 1);
+
     // 1. Reset GBTx0 and GBTx1
+    std::cout << "step 1" << std::endl;
     cs.sendGPIO(opc_ip, sca_address + ".gpio.gbtx0Rstn", 0);
     cs.sendGPIO(opc_ip, sca_address + ".gpio.gbtx1Rstn", 0);
+    sleep(1);
+    cs.sendGPIO(opc_ip, sca_address + ".gpio.gbtx0Rstn", 1);
+    cs.sendGPIO(opc_ip, sca_address + ".gpio.gbtx1Rstn", 1);
 
     sleep(1);
 
     // 2. and 3. set GBTx0 and GBTx1 registers (from config file GBTX_cfg_reg_extclk_rev02_110117.txt)
-
+    std::cout << "step 2 and 3" << std::endl;
     // vector from GBTX_cfg_reg_extclk_rev02_110117.txt
     std::vector<uint8_t> ADDCConfigurationData {
             0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
@@ -86,8 +99,8 @@ int main(int argc, const char *argv[])
 
     for (uint i=0; i<ADDCConfigurationData.size(); i++)
     {
-        gbtx_data[0] = ((uint8_t) ((i) >> 8));
-        gbtx_data[1] = ((uint8_t) ((i) & 0xff));
+        gbtx_data[1] = ((uint8_t) ((i) >> 8));
+        gbtx_data[0] = ((uint8_t) ((i) & 0xff));
         gbtx_data[2] = ADDCConfigurationData[i];
         cs.sendI2cRaw(opc_ip, sca_address + ".gbtx0.gbtx0", gbtx_data, gbtx_size);
         cs.sendI2cRaw(opc_ip, sca_address + ".gbtx1.gbtx1", gbtx_data, gbtx_size);
@@ -95,14 +108,23 @@ int main(int argc, const char *argv[])
 
 
     // 4. Reset ART0 and ART1
+    std::cout << "step 4" << std::endl;
     cs.sendGPIO(opc_ip, sca_address + ".gpio.art0SRstn", 0);
     cs.sendGPIO(opc_ip, sca_address + ".gpio.art1SRstn", 0);
     cs.sendGPIO(opc_ip, sca_address + ".gpio.art0CRstn", 0);
     cs.sendGPIO(opc_ip, sca_address + ".gpio.art1CRstn", 0);
     cs.sendGPIO(opc_ip, sca_address + ".gpio.art0Rstn", 0);
     cs.sendGPIO(opc_ip, sca_address + ".gpio.art1Rstn", 0);
+    sleep(1);
+    cs.sendGPIO(opc_ip, sca_address + ".gpio.art0SRstn", 1);
+    cs.sendGPIO(opc_ip, sca_address + ".gpio.art1SRstn", 1);
+    cs.sendGPIO(opc_ip, sca_address + ".gpio.art0CRstn", 1);
+    cs.sendGPIO(opc_ip, sca_address + ".gpio.art1CRstn", 1);
+    cs.sendGPIO(opc_ip, sca_address + ".gpio.art0Rstn", 1);
+    cs.sendGPIO(opc_ip, sca_address + ".gpio.art1Rstn", 1);
 
     // 6.a set the ART0 ASIC to provide continuous “010101…” 
+    std::cout << "step 6a" << std::endl;
     std::vector<uint8_t> ARTregisters {21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 2}; 
     std::vector<uint8_t> ARTregistervalues {0xff, 0x3f, 0x00, 0xf0, 0xff, 0x03, 0x00, 0xff, 0x3f, 0x00, 0xf0, 0xff, 0x03, 0x00, 0x80};
 
@@ -110,82 +132,91 @@ int main(int argc, const char *argv[])
     {
         art_data[0] = ARTregisters[i];
         art_data[1] = ARTregistervalues[i];
-        cs.sendI2cRaw(opc_ip, sca_address + ".art0Core", art_data, art_size);
+        cs.sendI2cRaw(opc_ip, sca_address + ".art0Core.art0Core", art_data, art_size);
     }
 
     // 6.b Set the GBTx0 to training mode
-    gbtx_data[0] = 0;
-    gbtx_data[1] = 62;
+    std::cout << "step 6b" << std::endl;
+    gbtx_data[1] = 0;
+    gbtx_data[0] = 62;
     gbtx_data[2] = 0x15;
     cs.sendI2cRaw(opc_ip, sca_address + ".gbtx0.gbtx0", gbtx_data, gbtx_size);
 
     // 6.c Enable the GBTx training for eport
+    std::cout << "step 6c" << std::endl;
     std::vector<uint8_t> GBTxregisters {78,79,80,102,103,104,126,127,128,150,151,152,174,175,176,198,199,200,222,223,224};
     for (uint i=0; i<GBTxregisters.size(); i++) 
     {
-        gbtx_data[0] = ((uint8_t) ((GBTxregisters[i]) >> 8)); 
-        gbtx_data[1] = ((uint8_t) ((GBTxregisters[i]) & 0xff));
+        gbtx_data[1] = ((uint8_t) ((GBTxregisters[i]) >> 8)); 
+        gbtx_data[0] = ((uint8_t) ((GBTxregisters[i]) & 0xff));
         gbtx_data[2] = 0xff;
         cs.sendI2cRaw(opc_ip, sca_address + ".gbtx0.gbtx0", gbtx_data, gbtx_size);
     }
 
     // 6.d fix the e-port phase
+    std::cout << "step 6d" << std::endl;
     sleep(1);
     for (uint i=0; i<GBTxregisters.size(); i++) 
     {
-        gbtx_data[0] = ((uint8_t) ((GBTxregisters[i]) >> 8)); 
-        gbtx_data[1] = ((uint8_t) ((GBTxregisters[i]) & 0xff));
+        gbtx_data[1] = ((uint8_t) ((GBTxregisters[i]) >> 8)); 
+        gbtx_data[0] = ((uint8_t) ((GBTxregisters[i]) & 0xff));
         gbtx_data[2] = 0x00;
         cs.sendI2cRaw(opc_ip, sca_address + ".gbtx0.gbtx0", gbtx_data, gbtx_size);
     }
 
 
     // 7.a repeat above for ART1 and GBTx0: set the ART1 ASIC to provide continuous “010101…” 
+    std::cout << "step 7a" << std::endl;
     for (uint i=0; i<ARTregisters.size(); i++)
     {
         art_data[0] = ARTregisters[i];
         art_data[1] = ARTregistervalues[i];
-        cs.sendI2cRaw(opc_ip, sca_address + ".art1Core", art_data, art_size);
+        cs.sendI2cRaw(opc_ip, sca_address + ".art1Core.art1Core", art_data, art_size);
     }
 
     // 7.b Set the GBTx0 to training mode
-    gbtx_data[0] = 0;
-    gbtx_data[1] = 62;
+    std::cout << "step 7b" << std::endl;
+    gbtx_data[1] = 0;
+    gbtx_data[0] = 62;
     gbtx_data[2] = 0x15;
     cs.sendI2cRaw(opc_ip, sca_address + ".gbtx1.gbtx1", gbtx_data, gbtx_size);
 
     // 7.c Enable the GBTx training for eport
+    std::cout << "step 7c" << std::endl;
     for (uint i=0; i<GBTxregisters.size(); i++) 
     {
-        gbtx_data[0] = ((uint8_t) ((GBTxregisters[i]) >> 8)); 
-        gbtx_data[1] = ((uint8_t) ((GBTxregisters[i]) & 0xff));
+        gbtx_data[1] = ((uint8_t) ((GBTxregisters[i]) >> 8)); 
+        gbtx_data[0] = ((uint8_t) ((GBTxregisters[i]) & 0xff));
         gbtx_data[2] = 0xff;
         cs.sendI2cRaw(opc_ip, sca_address + ".gbtx1.gbtx1", gbtx_data, gbtx_size);
     }
 
     // 7.d fix the e-port phase
+    std::cout << "step 7d" << std::endl;
     sleep(1);
     for (uint i=0; i<GBTxregisters.size(); i++) 
     {
-        gbtx_data[0] = ((uint8_t) ((GBTxregisters[i]) >> 8)); 
-        gbtx_data[1] = ((uint8_t) ((GBTxregisters[i]) & 0xff));
+        gbtx_data[1] = ((uint8_t) ((GBTxregisters[i]) >> 8)); 
+        gbtx_data[0] = ((uint8_t) ((GBTxregisters[i]) & 0xff));
         gbtx_data[2] = 0x00;
         cs.sendI2cRaw(opc_ip, sca_address + ".gbtx1.gbtx1", gbtx_data, gbtx_size);
     }
 
     // 8. bit order alignment
     // 8.a 
+    std::cout << "step 8a" << std::endl;
     art_data[0] = 2;
     art_data[1] = 0x10;
-    cs.sendI2cRaw(opc_ip, sca_address + ".art0Core", art_data, art_size);
+    cs.sendI2cRaw(opc_ip, sca_address + ".art0Core.art0Core", art_data, art_size);
 
     // 8.b Mask off the ART input channels to avoid interferences
+    std::cout << "step 8b" << std::endl;
     std::vector<uint8_t> ARTCoreregisters {9, 10, 11, 12};
     for (uint i=0; i<ARTCoreregisters.size(); i++)
     {
         art_data[0] = ARTCoreregisters[i];
         art_data[1] = 0xff;
-        cs.sendI2cRaw(opc_ip, sca_address + ".art0core", art_data, art_size);
+        cs.sendI2cRaw(opc_ip, sca_address + ".art0Core.art0Core", art_data, art_size);
     }
 
     // 8.c Check the received ART data on the Trigger processor end, scan the phase of the GBTx0 160M clock. 
@@ -197,64 +228,68 @@ int main(int argc, const char *argv[])
 
     // rest: manually...
 
-    for (uint i=0; i<256; i++) 
-    {
-        gbtx_data[0] = 0; 
-        gbtx_data[1] = 8;
-        gbtx_data[2] = i;
-        cs.sendI2cRaw(opc_ip, sca_address + ".gbtx0.gbtx0", gbtx_data, gbtx_size);
-        sleep(2);
-    }
+    // for (uint i=0; i<256; i++) //exact number??
+    // {
+    //     gbtx_data[1] = 0; 
+    //     gbtx_data[0] = 8;
+    //     gbtx_data[2] = i;
+    //     cs.sendI2cRaw(opc_ip, sca_address + ".gbtx0.gbtx0", gbtx_data, gbtx_size);
+    //     sleep(2);
+    // }
 
 
     // 9. repeat bit order alignment on ART1/GBTx1
     // 9.a 
+    std::cout << "step 9a" << std::endl;
     art_data[0] = 2;
     art_data[1] = 0x10;
-    cs.sendI2cRaw(opc_ip, sca_address + ".art1Core", art_data, art_size);
+    cs.sendI2cRaw(opc_ip, sca_address + ".art1Core.art1Core", art_data, art_size);
 
     // 9.b Mask off the ART input channels to avoid interferences
+    std::cout << "step 9b" << std::endl;
     for (uint i=0; i<ARTCoreregisters.size(); i++)
     {
         art_data[0] = ARTCoreregisters[i];
         art_data[1] = 0xff;
-        cs.sendI2cRaw(opc_ip, sca_address + ".art1core", art_data, art_size);
+        cs.sendI2cRaw(opc_ip, sca_address + ".art1Core.art1Core", art_data, art_size);
     }
 
     // 9.c Check the received ART data on the Trigger processor end, scan the phase of the GBTx1 160M clock. 
     // To do that, write the phase value to the GBTx1 register 8, 
     // check the output 112 bit data until the 12-bit BCID counter be found in bit 107~96 and the rest bits are fixed. --> ?????
-
-    for (uint i=0; i<256; i++) 
-    {
-        gbtx_data[0] = 0; 
-        gbtx_data[1] = 8;
-        gbtx_data[2] = i;
-        cs.sendI2cRaw(opc_ip, sca_address + ".gbtx1.gbtx1", gbtx_data, gbtx_size);
-        sleep(2);
-    }
+    std::cout << "step 9c" << std::endl;
+    // for (uint i=0; i<256; i++) 
+    // {
+    //     gbtx_data[1] = 0; 
+    //     gbtx_data[0] = 8;
+    //     gbtx_data[2] = i;
+    //     cs.sendI2cRaw(opc_ip, sca_address + ".gbtx1.gbtx1", gbtx_data, gbtx_size);
+    //     sleep(2);
+    // }
 
     // 10.
+    std::cout << "step 10" << std::endl;
     for (uint i=0; i<ARTCoreregisters.size(); i++)
     {
         art_data[0] = ARTCoreregisters[i]; 
         art_data[1] = 0x00;
-        cs.sendI2cRaw(opc_ip, sca_address + ".art0core", art_data, art_size);
-        cs.sendI2cRaw(opc_ip, sca_address + ".art1core", art_data, art_size);
+        cs.sendI2cRaw(opc_ip, sca_address + ".art0Core.art0Core", art_data, art_size);
+        cs.sendI2cRaw(opc_ip, sca_address + ".art1Core.art1Core", art_data, art_size);
     }
 
     // anything else needed?
 
     // Yes: bits for BCR counters
     // 11 
+    std::cout << "step 11" << std::endl;
     art_data[0] = 14;
     art_data[1] = 0xF;
-    cs.sendI2cRaw(opc_ip, sca_address + ".art0Core", art_data, art_size);
-    cs.sendI2cRaw(opc_ip, sca_address + ".art1Core", art_data, art_size);
+    cs.sendI2cRaw(opc_ip, sca_address + ".art0Core.art0Core", art_data, art_size);
+    cs.sendI2cRaw(opc_ip, sca_address + ".art1Core.art1Core", art_data, art_size);
     art_data[0] = 15;
     art_data[1] = 0xFF;
-    cs.sendI2cRaw(opc_ip, sca_address + ".art0Core", art_data, art_size);
-    cs.sendI2cRaw(opc_ip, sca_address + ".art1Core", art_data, art_size);
+    cs.sendI2cRaw(opc_ip, sca_address + ".art0Core.art0Core", art_data, art_size);
+    cs.sendI2cRaw(opc_ip, sca_address + ".art1Core.art1Core", art_data, art_size);
 
     return 0;
 
