5a9a75830209b2491579a7a7120e57ebf2dbf661
bb5user_local <tuna@cern.ch>
2019-04-24 16:12:48 +0200

    steer the scope too!

--- app/read_vmm_adc_slowly.cpp
+++ app/read_vmm_adc_slowly.cpp
@@ -28,6 +28,9 @@ int main(int ac, const char *av[]) {
     int sleep_time;
     int targeted_vmm_id;
     int targeted_channel_id;
+    int scope_channel;
+    int scope_n;
+    int scope_overwrite;
     std::string config_filename;
     std::string fe_name;
     po::options_description desc(description);
@@ -50,6 +53,12 @@ int main(int ac, const char *av[]) {
         default_value(-1), "VMM channel to read (otherwise: loop)")
         ("sleep", po::value<int>(&sleep_time)->
         default_value(1), "Time for sleeping between measurements")
+        ("screens", po::value<int>(&scope_n)->
+        default_value(1), "Number of scope screenshots to read")
+        ("scope_channel", po::value<int>(&scope_channel)->
+        default_value(2), "Which scope channel to read")
+        ("scope_overwrite", po::value<int>(&scope_overwrite)->
+        default_value(1), "The output file of scope measurements: dont overwrite (0) or overwrite (1)")
       ;
 
     po::variables_map vm;
@@ -135,6 +144,26 @@ int main(int ac, const char *av[]) {
                       << " " << results[i] 
                       << std::endl;
           }
+
+          //
+          // read the scope
+          //
+
+          boost::format ch_fmt("%02d");
+          ch_fmt % channel_id;
+
+          std::string n_screens  = " -n " + std::to_string(scope_n);
+          std::string sc_channel = " -c " + std::to_string(scope_channel);
+          std::string overwrite  = scope_overwrite ? " -r " : "";
+          std::string outputfile = "scope_" + feb.getAddress() 
+            + "_VMM_" + std::to_string(vmm_id) 
+            + "_CH_"  + ch_fmt.str() 
+            + ".dat";
+          std::string output_ops = " -o " + outputfile;
+          std::string ops        = n_screens + sc_channel + overwrite + output_ops;
+          std::string cmd        = "python bb5_analysis/scripts/BB5ScopeReadout.py " + ops;
+          std::cout << "RUN: " << cmd << std::endl;
+          system(cmd.c_str());
 
           time_now_ms = chr::duration_cast<chr::milliseconds>
             (chr::system_clock::now().time_since_epoch()).count();
