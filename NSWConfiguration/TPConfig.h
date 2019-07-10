1297f5bf52c8f83309c468b77dbb591f50b1e490
Noam Inbari <ninbari@lxplus798.cern.ch>
2019-07-10 14:00:27 +0200

    The first drop of adding TP register setting support. I2cRegisterMappingsTP.h auto-generated code from CSV file, which TPConfig.cpp is based on

--- NSWConfiguration/TPConfig.h
+++ NSWConfiguration/TPConfig.h
@@ -1,3 +1,4 @@
+
 /// MMFE8 Configuration class
 
 #ifndef NSWCONFIGURATION_TPCONFIG_H_
@@ -15,7 +16,7 @@
 #include "NSWConfiguration/Utility.h"
 #include "NSWConfiguration/FEConfig.h"
 #include "NSWConfiguration/I2cMasterConfig.h"
-#include "NSWConfiguration/I2cRegisterMappings.h"
+#include "NSWConfiguration/I2cRegisterMappingsTP.h"
 
 using boost::property_tree::ptree;
 
@@ -29,48 +30,23 @@ namespace nsw {
 //! Used as base class for MMFE8, PFEB and SBEF Configurations
 
 class TPConfig: public FEConfig {
- private:
-    // std::vector<VMMConfig> m_vmms;
-    // I2cMasterConfig m_tp_analog;
-    // I2cMasterConfig m_tp_digital;
-    std::vector<I2cMasterConfig> m_config_elements;
-    // std::vector<std::string> m_gpios;  // List of GPIO names in the FEB
-
- public:
+private:
+    std::map<std::string,I2cMasterConfig*> m_registerFiles;
+    I2cMasterConfig *m_I2cMasterConfigPtrArr[NUM_REGISTER_FILES] = {NULL};
+public:
     //! Constructor.
     //! The ptree in the argument should contain
     //! - OpcServerIp, OpcNodeId
-    //! - rocPllCoreAnalog, rocCoreDigital
-    //! - Multiple vmm instances named vmm0 to vmmN
-    //! - Multiple tds instances named tds0 to tdsN (optional)
+  
     explicit TPConfig(ptree config);
-    ~TPConfig() {}
-
+    ~TPConfig() ;
+        
+    uint32_t getRegisterValue(std::string master, std::string slave, std::string register_name);        
+    void setRegisterValue(std::string master, std::string slave, std::string register_name, uint32_t value);
     void dump();
-
-    // const std::vector<VMMConfig> & getVmms() const {return m_vmms;}
-    // const I2cMasterConfig & getTpAnalog() const {return m_tp_analog;}
-    // const I2cMasterConfig & getTpDigital() const {return m_tp_digital;}
-
-    // this will need to be renamed once we understand what each master corresponds to
-    const std::vector<I2cMasterConfig> & getConfigElements() const {return m_config_elements;}
-
-    // I2cMasterConfig & getTpAnalog() {
-    //     return const_cast<I2cMasterConfig &>(static_cast<const TPConfig&>(*this).getTpAnalog());
-    // }
-
-    // I2cMasterConfig & getTpDigital() {
-    //     return const_cast<I2cMasterConfig &>(static_cast<const TPConfig&>(*this).getTpDigital());
-    // }
-
-    // std::vector<VMMConfig> & getVmms() {
-    //     return const_cast<std::vector<VMMConfig>&>(static_cast<const TPConfig&>(*this).getVmms());
-    // }
-
-    std::vector<I2cMasterConfig> & getConfigElements() {
-        return const_cast<std::vector<I2cMasterConfig>&>(static_cast<const TPConfig&>(*this).getConfigElements());
-    }
 };
 }  // namespace nsw
 
 #endif  // NSWCONFIGURATION_TPCONFIG_H_
+
+
