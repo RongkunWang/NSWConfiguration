aa6fb9a4e656f6316dd4259dff988a46f012c0df
lawrenceleejr <lawrenceleejr@gmail.com>
2019-08-05 15:44:44 +0200

    TP: small updates

--- app/tp_comm_test.cpp
+++ app/tp_comm_test.cpp
@@ -39,7 +39,7 @@ int main(int argc, const char *argv[])
             "message to write in hex (Default: 0xC0FFEEEE)")
         ("slaveAddr,s", po::value<std::string>(&slaveAddr)->default_value("NSW_TrigProc_STGC.I2C_0.bus0"),
             "slave bus to write to(Default: NSW_TrigProc_STGC.I2C_0.bus0)")
-        ("regAddr,r", po::value<std::string>(&regAddr)->default_value("0x2AD"),
+        ("regAddr,r", po::value<std::string>(&regAddr)->default_value("0x000"),
             "register to read from/write to(Default: 0x2AD)")
         ("opc_ip,o", po::value<std::string>(&opc_ip)->default_value("pcatlnswfelix01.cern.ch:48020"),
             "hostname for OPC server");
@@ -63,7 +63,7 @@ int main(int argc, const char *argv[])
     // auto opc_ip = tp.getOpcServerIp();
     // auto sca_tp_address = tp.getAddress();
 
-    int test = 1;
+    int test = 0;
 
     std::vector<uint8_t> data;
     std::vector<uint8_t> outdata;
@@ -71,7 +71,7 @@ int main(int argc, const char *argv[])
     std::vector<uint8_t> regAddrVec;
 
     regAddrVec = nsw::hexStringToByteVector(regAddr,4,true);
-    std::cout << ">>> Register address array: ";
+    std::cout << "... Register address array: ";
     for (uint i=0; i<regAddrVec.size(); i++) {
         std::cout << std::hex << unsigned(regAddrVec[i]) << " ";
     }
@@ -79,8 +79,9 @@ int main(int argc, const char *argv[])
 
     if (readMode) {
         if(test==0){
-            std::cout << "Testing the readout of a register via I2c..." << std::endl;
+            std::cout << "... Testing the readout of a register via I2c..." << std::endl;
             outdata = cs.readI2cAtAddress(opc_ip, slaveAddr, regAddrVec.data(), regAddrVec.size(), 4);
+            // outdata = cs.readI2cAtAddress("pcatlnswfelix01.cern.ch:48020", "NSW_TrigProc_STGC.I2C_0.bus0", regAddrVec.data(), regAddrVec.size(), 4);
             for (uint i=0; i<outdata.size(); i++) {
                 std::cout << std::hex << unsigned(outdata[i]) << std::endl;
             }
@@ -96,8 +97,8 @@ int main(int argc, const char *argv[])
         // Already have the first four bytes of vector stored as regAddrVec.
         // Just need to encode the message.
         data = nsw::hexStringToByteVector(message,4,true);
-        std::cout << ">>> Message to write: " << message << std::endl;
-        std::cout << ">>> ... in a byte vector: ";
+        std::cout << "... Message to write: " << message << std::endl;
+        std::cout << "... ... in a byte vector: ";
         for (uint i=0; i<data.size(); i++) {
             std::cout << std::hex << unsigned(data[i]) << " ";
         }
@@ -109,7 +110,7 @@ int main(int argc, const char *argv[])
         if(test==0){
             cs.sendI2cRaw(opc_ip, slaveAddr, entirePayload.data(), entirePayload.size() );
             if (readMode){
-                std::cout << ">>> Reading data back from register: " << std::endl;
+                std::cout << "... Reading data back from register: " << std::endl;
                 outdata = cs.readI2cAtAddress(opc_ip, slaveAddr, regAddrVec.data(), regAddrVec.size(), 4);
                 for (uint i=0; i<outdata.size(); i++) {
                     std::cout << std::hex << unsigned(outdata[i]) << std::endl;
@@ -118,6 +119,12 @@ int main(int argc, const char *argv[])
         }
 
     }
+
+
+    // tp = nsw::TPConfig();
+
+    std::cout << "... Done with TP Comm test" << std::endl;
+
 
     return 0;
 
