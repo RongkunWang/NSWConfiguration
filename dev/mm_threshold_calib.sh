bc8377f21e5f81572dcc11778608ce01e42dcb6b
alexandertuna <alexander.tuna@gmail.com>
2019-08-07 12:26:37 +0200

    putting everything in one script

--- dev/mm_threshold_calib.sh
+++ dev/mm_threshold_calib.sh
@@ -8,29 +8,61 @@ MMFE8S="${OLDSLICE}"
 # MMFE8S="MMFE8_ML1P4_IPR"
 #
 
-CONFIG="/afs/cern.ch/user/n/nswdaq/public/sw/config-ttc/config-files/mmfe8_bb5_16_boards_l1matching_nobypass_712_phase1_cosmicsSlice_cosmics.json"
+FILES="/afs/cern.ch/user/n/nswdaq/public/sw/config-ttc/config-files"
+CONFIG="${FILES}/mmfe8_bb5_16_boards_l1matching_nobypass_712_phase1_cosmicsSlice_cosmics.json"
 NSAMPS="100"
 
-SCRIPT_CONFIG="./x86_64-centos7-gcc8-opt/NSWConfiguration/configure_frontend"
-SCRIPT_MEASUR="./x86_64-centos7-gcc8-opt/NSWConfiguration/read_channel_thresholds"
-SCRIPT_CALIBR="./x86_64-centos7-gcc8-opt/NSWConfiguration/calibrate_trimmers_multirms"
-SCRIPT_INJECT="./bb5_analysis/scripts/injectTrimmerCalibration.py"
-
 # NOW="2019_07_16_21h00m00s"
-NOW=$(date +%Y_%m_%d_%Hh%Mm%Ss)
 RMS="12,10,8,6,4"
+NOW=$(date +%Y_%m_%d_%Hh%Mm%Ss)
+OUT=trimmers/$NOW
 
-mkdir -p trimmers/${NOW}
+WEB=/eos/atlas/atlascerngroupdisk/det-nsw/bb5/cosmics/html/trimmers/${NOW}
+LOG=/eos/atlas/atlascerngroupdisk/det-nsw/bb5/cosmics/html/trimmers/index.html
+ROOT2HTML="/eos/atlas/atlascerngroupdisk/det-nsw/bb5/cosmics/html/root2html.py"
+
+BIN="./x86_64-centos7-gcc8-opt/NSWConfiguration"
+CALIBR="${BIN}/calibrate_trimmers_multirms"
+INJECT="./bb5_analysis/scripts/injectTrimmerCalibration.py"
+
+mkdir -p ${OUT}
 
 #
 # ALWAYS CONFIGURE
 #
-$SCRIPT_CONFIG -c $CONFIG -v -r
+$BIN/configure_frontend -c $CONFIG -v -r
 
-# calibrate thresholds and trimmers, 2019
-time python3 NSWConfiguration/app/threshold_parallel.py -rms ${RMS} -c $CONFIG -s $SCRIPT_CALIBR -f $MMFE8S
+# calibrate thresholds
+time python3 NSWConfiguration/app/threshold_parallel.py -rms $RMS -c $CONFIG -s $CALIBR -f $MMFE8S
 for R in $(echo ${RMS} | tr "," "\n"); do
-    grep -h "^DATA_x${R}" thresholds_MMFE8_ML* > trimmers/${NOW}/trimmers_x${R}.txt
-    python $SCRIPT_INJECT -i $CONFIG -c trimmers/${NOW}/trimmers_x${R}.txt -o trimmers/${NOW}/trimmers_x${R}.json -d 0
+    grep -h "^DATA_x${R}" thresholds_MMFE8_ML* > $OUT/trimmers_x${R}.txt
+    python $INJECT -i $CONFIG -c $OUT/trimmers_x${R}.txt -o ${OUT}/trimmers_x${R}.json -d 0
 done
 
+# baselines
+grep -h "^DATA" baselines_MMFE8_ML*VMM* > $OUT/baselines.txt
+
+# below: make fancy plots
+for R in $(echo ${RMS} | tr "," "\n"); do
+
+    # thresholds
+    CONFIG="$OUT/trimmers_x${R}.json"
+    $BIN/read_channel_monitor -c $CONFIG -s  10 -o $OUT/$R/thresholds_trimmed   --threshold --dump -n $MMFE8S
+    $BIN/read_channel_monitor -c $CONFIG -s  10 -o $OUT/$R/thresholds_untrimmed --threshold --dump -n $MMFE8S -T 0
+    grep -h "^DATA" $OUT/$R/thresholds_trimmed/*.txt   > $OUT/$R/thresholds_trimmed.txt
+    grep -h "^DATA" $OUT/$R/thresholds_untrimmed/*.txt > $OUT/$R/thresholds_untrimmed.txt
+
+    ## plotting script
+    python NSWConfiguration/dev/mm_trimmers.plot.py -o $OUT/trimmers_${NOW}_x${R}.root \
+        --bl           $OUT/baselines.txt \
+        --th_untrimmed $OUT/$R/thresholds_untrimmed.txt \
+        --th_trimmed   $OUT/$R/thresholds_trimmed.txt
+
+    ## copy to the web
+    $ROOT2HTML $OUT/trimmers_${NOW}_x${R}.root
+    mv $OUT/trimmers_${NOW}_x${R} ${WEB}_x${R}
+    echo "<a href='${NOW}_x${R}'> ${NOW}_x${R} </a> :: INSERT DESCRIPTION HERE<br/>" >> $LOG
+    echo "Update the log, please: $LOG"
+    echo "www.cern.ch/BB5Cosmics/html/trimmers/"
+
+done
