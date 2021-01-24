5c63ad3b75a47689a947b54d20c2e94f4f09f7b6
yzach <yuval.zach@cern.ch>
2021-01-24 17:41:34 +0100

    Improve write_tp_bram error messages

--- app/write_tp_bram.cpp
+++ app/write_tp_bram.cpp
@@ -28,9 +28,9 @@ int main(int argc, char** argv) {
         ("help,h", "Display help message")
         ("configfile,c", po::value<std::string>(&config_filename)->required(), "Configuration file path")
         ("memfile,m", po::value<std::string>(&mem_filename)->required(), "MEM file path")
-        ("dry-run", po::bool_switch(&dry_run)->default_value(false), "DON'T send bRAM contents to trigger processor")
+        ("dryrun", po::bool_switch(&dry_run)->default_value(false), "DON'T send bRAM contents to trigger processor")
         ("tp,t", po::value<std::string>(&tp_name)->required(), "Name of trigger processor");
-
+    // additional LUT name parameter, used to get I2C address from .h file generated from the csv
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, po_desc), vm);
     
@@ -40,14 +40,14 @@ int main(int argc, char** argv) {
     }
     try {
         po::notify(vm);
-    } catch (const po::required_option& ex) {
+    // Why does this not catch an unrecognised option error? 
+    } catch (const po::error& ex) {
         std::cout << ex.what() << std::endl;
         return 1;
     }
 
     std::cout << "Parsing memory file..." << std::endl;
     const auto memory = parse_memfile(config_filename);
-    
     std::cout << "Parsing configuration file..." << std::endl;
     auto tp_config = get_tp_config(config_filename, tp_name);
 
@@ -58,8 +58,14 @@ int main(int argc, char** argv) {
 }
 
 std::map<uint16_t, std::vector<uint32_t>> parse_memfile(const std::string& filename) {
-    MemfileReader mr{ filename, '\n' };
-    return mr.read();
+    try {
+        MemfileReader mr{ filename, '\n' };
+        return mr.read();
+    } catch (std::exception& ex) {
+        std::cout << "Can't parse memory file file due to error: " << ex.what() << std::endl;
+        std::cout << "Exiting..." << std::endl;
+        std::exit(1);
+    }
 }
 
 void write_bram(const std::map<uint16_t, std::vector<uint32_t>>& contents, const std::string& opc_server,
@@ -85,7 +91,7 @@ nsw::TPConfig get_tp_config(const std::string& config_file, const std::string& t
         tp_config_tree = config_reader.readConfig(tp_name);
     }
     catch (const std::exception& ex) {
-        std::cout << "Can't read config file due to : " << ex.what() << std::endl;
+        std::cout << "Can't read config file due to error: " << ex.what() << std::endl;
         std::cout << "Exiting..." << std::endl;
         std::exit(1);
     }
