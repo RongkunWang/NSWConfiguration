46526507c247a0c32c86f91ed0b79c02adb3addf
Jared Sturdy <sturdy@cern.ch>
2021-08-06 17:08:49 +0200

    [PadTrigger] PadTrigger gut interface

--- NSWConfiguration/hw/PadTrigger.h
+++ NSWConfiguration/hw/PadTrigger.h
@@ -8,77 +8,67 @@
 
 #include "NSWConfiguration/PadTriggerSCAConfig.h"
 
-namespace nsw {
-  namespace hw {
+namespace nsw::hw {
+  /**
+   * \brief Class representing a Pad Trigger
+   *
+   * Provides methods to read/write individual PadTrigger registers,
+   * as well as to write a complete configuration and read back all
+   * the registers contained in the configuration.
+   *
+   * Register mapping: TODO
+   * Documentation: TODO
+   */
+  class PadTrigger
+  {
+  public:
     /**
-     * \brief Class representing a Pad Trigger
-     *
-     * Provides methods to read/write individual PadTrigger registers,
-     * as well as to write a complete configuration and read back all
-     * the registers contained in the configuration.
+     * \brief Constrctor from a \ref PadTriggerConfig object
      */
-    class PadTrigger
-    {
-    public:
-      /**
-       * \brief Constrctor from a \ref PadTriggerConfig object
-       */
-      explicit PadTrigger(const PadTriggerSCAConfig& config);
+    explicit PadTrigger(const PadTriggerSCAConfig& config);
 
-      /**
-       * \brief Read the full PadTrigger address space
-       *
-       * \returns a map of address to register value
-       */
-      std::map<std::uint8_t, std::vector<std::uint32_t>> readConfiguration();
+    /**
+     * \brief Read the full PadTrigger address space
+     *
+     * \returns a map of address to register value
+     */
+    [[nodiscard]] std::map<std::uint8_t, std::vector<std::uint32_t>> readConfiguration() const;
 
-      /**
-       * \brief Write the full PadTrigger configuration
-       */
-      void writeConfiguration();
+    /**
+     * \brief Write the full PadTrigger configuration
+     */
+    void writeConfiguration() const;
 
-      /**
-       * \brief Write a value to a PadTrigger register address
-       *
-       * \param regAddress is the address of the register
-       * \param scaNode is the address of the SCA that this PadTrigger is connected to
-       * \param data is an std::vector containing the bytes to be written to the adddress
-       *        - SCAControlRegister has size 4 bytes
-       *        - RouterConfigRegister has size 2 bytes
-       *        - VTTxControlRegister has size 2 bytes
-       */
-      void writeRegister(std::uint8_t regAddress,
-                         const std::string& scaNode,
-                         const std::vector<std::uint8_t>& data);
+    /**
+     * \brief Write a value to a TP register address
+     *
+     * \param regAddress is the address of the register
+     * \param value is the value to be written
+     */
+    void writeRegister([[maybe_unused]] std::uint8_t regAddress,
+                       [[maybe_unused]] std::uint32_t value) const;
 
-      /**
-       * \brief Read an individual PadTrigger register by its address
-       *
-       * \param regAddress is the name of the register node
-       * \param scaNode is the address of the SCA that this PadTrigger is connected to
-       * \param bytesToRead is the number of bytes to read out of the adddress
-       *        - SCAControlRegister has size 4 bytes
-       *        - RouterConfigRegister has size 2 bytes
-       *        - VTTxControlRegister has size 2 bytes
-       */
-      std::vector<std::uint8_t> readRegister(std::uint8_t regAddress,
-                                             const std::string& scaNode,
-                                             std::size_t bytesToRead);
+    /**
+     * \brief Read an individual TP register by its address
+     *
+     * \param regAddress is the address of the register
+     */
+    [[nodiscard]] std::vector<std::uint8_t> readRegister(
+      [[maybe_unused]] std::uint8_t regAddress) const;
 
-      /**
-       * \brief Get the \ref PadTriggerConfig object associated with this PadTrigger object
-       *
-       * Both const and non-const overloads are provided
-       */
-      [[nodiscard]] PadTriggerSCAConfig& getConfig() { return m_config; }
-      [[nodiscard]] const PadTriggerSCAConfig& getConfig() const { return m_config; }  //!< overload
+    /**
+     * \brief Get the \ref PadTriggerConfig object associated with this PadTrigger object
+     *
+     * Both const and non-const overloads are provided
+     */
+    [[nodiscard]] PadTriggerSCAConfig& getConfig() { return m_config; }
+    [[nodiscard]] const PadTriggerSCAConfig& getConfig() const { return m_config; }  //!< overload
 
-    private:
-      PadTriggerSCAConfig m_config;  //!< PadTriggerConfig object associated with this PadTrigger
-      std::string m_opcserverIp;     //!< Address and port of OPC Server
-      std::string m_scaAddress;      //!< SCA address of PadTrigger item in the OPC address space
-    };
-  }  // namespace hw
-}  // namespace nsw
+  private:
+    PadTriggerSCAConfig m_config;  //!< PadTriggerConfig object associated with this PadTrigger
+    std::string m_opcserverIp;     //!< Address and port of OPC Server
+    std::string m_scaAddress;      //!< SCA address of PadTrigger item in the OPC address space
+  };
+}  // namespace nsw::hw
 
 #endif
--- src/hw/PadTrigger.cpp
+++ src/hw/PadTrigger.cpp
@@ -1,181 +1,29 @@
-#include "NSWConfiguration/PadTriggerInterface.h"
+#include "NSWConfiguration/hw/PadTrigger.h"
 
-#include <iterator>
 #include <stdexcept>
-#include <string>
-#include <chrono>
-
-#include "NSWConfiguration/Constants.h"
-#include "NSWConfiguration/OpcManager.h"
-#include "NSWConfiguration/SCAInterface.h"
-#include "NSWConfiguration/Utility.h"
-
-using namespace std::chrono_literals;
 
 nsw::hw::PadTrigger::PadTrigger(const PadTriggerSCAConfig& config) :
   m_config(config), m_opcserverIp(config.getOpcServerIp()), m_scaAddress(config.getAddress())
 {}
 
-void nsw::hw::PadTrigger::writeConfiguration()
-{
-  const auto& opcconnection = OpcManager::getConnection(m_opcserverIp);
-  // Repeaters
-  if (m_config.ConfigRepeaters()) {
-    const std::vector<std::tuple<std::string, uint8_t, uint8_t>> repeater_sequence_of_commands = {
-      // clang-format off
-      // ENABLE SMBUS REGISTERS ON ALL REPEATER CHIPS
-      {"1", 0x07, 0x01}, {"1", 0x07, 0x11}, {"1", 0x07, 0x21}, {"1", 0x07, 0x31},
-      {"2", 0x07, 0x01}, {"2", 0x07, 0x11}, {"2", 0x07, 0x21}, {"2", 0x07, 0x31},
-      {"3", 0x07, 0x01}, {"3", 0x07, 0x11}, {"3", 0x07, 0x21}, {"3", 0x07, 0x31},
-      {"4", 0x07, 0x01}, {"4", 0x07, 0x11}, {"4", 0x07, 0x21}, {"4", 0x07, 0x31},
-      {"5", 0x07, 0x01}, {"5", 0x07, 0x11}, {"5", 0x07, 0x21}, {"5", 0x07, 0x31},
-      {"6", 0x07, 0x01}, {"6", 0x07, 0x11}, {"6", 0x07, 0x21}, {"6", 0x07, 0x31},
-      // SET REPEATER CHIPS EQUALISER SETTINGS
-      {"1", 0x14, 0x03}, {"1", 0x16, 0x03}, {"1", 0x18, 0x03}, {"1", 0x1A, 0x03},
-      {"2", 0x14, 0x03}, {"2", 0x16, 0x03}, {"2", 0x18, 0x03}, {"2", 0x1A, 0x03},
-      {"3", 0x14, 0x03}, {"3", 0x16, 0x03}, {"3", 0x18, 0x03}, {"3", 0x1A, 0x03},
-      {"4", 0x14, 0x03}, {"4", 0x16, 0x03}, {"4", 0x18, 0x03}, {"4", 0x1A, 0x03},
-      {"5", 0x14, 0x03}, {"5", 0x16, 0x03}, {"5", 0x18, 0x03}, {"5", 0x1A, 0x03},
-      {"6", 0x14, 0x03}, {"6", 0x16, 0x03}, {"6", 0x18, 0x03}, {"6", 0x1A, 0x03},
-      // DISABLE SMBUS REGISTERS ON ALL REPEATER CHIPS
-      // (not really necessary but it’s a protection against occasional commands sent by mistake)
-      {"1", 0x07, 0x00}, {"1", 0x07, 0x10}, {"1", 0x07, 0x20}, {"1", 0x07, 0x30},
-      {"2", 0x07, 0x00}, {"2", 0x07, 0x10}, {"2", 0x07, 0x20}, {"2", 0x07, 0x30},
-      {"3", 0x07, 0x00}, {"3", 0x07, 0x10}, {"3", 0x07, 0x20}, {"3", 0x07, 0x30},
-      {"4", 0x07, 0x00}, {"4", 0x07, 0x10}, {"4", 0x07, 0x20}, {"4", 0x07, 0x30},
-      {"5", 0x07, 0x00}, {"5", 0x07, 0x10}, {"5", 0x07, 0x20}, {"5", 0x07, 0x30},
-      {"6", 0x07, 0x00}, {"6", 0x07, 0x10}, {"6", 0x07, 0x20}, {"6", 0x07, 0x30},
-      // clang-format on
-    };
-
-    for (const auto& [rep, address, value] : repeater_sequence_of_commands) {
-      // values
-      const auto node = ".repeaterChip" + rep + ".repeaterChip" + rep;
-      const std::vector<uint8_t> data = {value};
-
-      // GPIO enable
-      DeviceInterface::SCA::sendGPIO(
-        opcconnection, m_scaAddress + ".gpio.gpio-repeaterChip" + rep, 1);
-
-      // Repeater I2C: write
-      ERS_INFO(m_opcserverIp << " " << m_scaAddress << ": Repeater I2C. "
-                             << "Writing 0x" << std::hex << static_cast<unsigned>(value)
-                             << " to address 0x" << static_cast<unsigned>(address) << std::dec);
-      writeRegister(address, node, data);
-      std::this_thread::sleep_for(10ms);
-
-      // Repeater I2C: readback
-      // FIXME TODO 1 is magic, number of bytes to read back from a Repeater address
-      const auto val = readRegister(address, node, 1).at(0);
-      ERS_INFO(m_opcserverIp << " " << m_scaAddress << ": Repeater I2C. "
-                             << " Readback " << rep << ": 0x" << std::hex << val);
-      std::this_thread::sleep_for(10ms);
-
-      // GPIO disable
-      DeviceInterface::SCA::sendGPIO(
-        opcconnection, m_scaAddress + ".gpio.gpio-repeaterChip" + rep, 0);
-    }
-  } else {
-    ERS_INFO("Skipping configuration of repeaters of " << m_opcserverIp << " " << m_scaAddress);
-  }
-
-  // VTTX
-  if (m_config.ConfigVTTx()) {
-    constexpr uint8_t address = 0x0;           // FIXME TODO address is magic
-    const std::vector<uint8_t> data = {0xC7};  // FIXME TODO configuration data is magic
-    const std::vector<std::string> vttxs = {"1", "2"};
-
-    // 2.0 VTTX
-    ERS_INFO("VTTx I2C: Writing " << std::hex << static_cast<std::uint32_t>(data.at(0))
-                                  << " to address 0x" << static_cast<std::uint32_t>(address)
-                                  << std::dec);
-    for (const auto& vttx : vttxs) {
-      const std::string node = ".vttx" + vttx + ".vttx" + vttx;
-      writeRegister(address, node, data);
-      std::this_thread::sleep_for(100ms);
-    }
-
-    // 2.1 Read them
-    for (const auto& vttx : vttxs) {
-      const std::string node = ".vttx" + vttx + ".vttx" + vttx;
-      // FIXME 2 is a magic number, bytes to read from a VTTx config register
-      const auto val =
-        nsw::byteVectorToWord32(readRegister(address, node, 2), nsw::padtrigger::SCA_LITTLE_ENDIAN);
-      ERS_INFO(m_opcserverIp << " " << m_scaAddress << " Readback VTTx" << vttx << ": " << std::hex
-                             << val << std::dec);
-      std::this_thread::sleep_for(100ms);
-    }
-  } else {
-    ERS_INFO("Skipping configuration of VTTx of " << m_opcserverIp << " " << m_scaAddress);
-  }
-
-  // Control register
-  if (m_config.ConfigControlRegister()) {
-    constexpr std::uint8_t address = 0x0;
-    const std::string node = ".fpga.fpga";
-    const auto i2c_val_32 = static_cast<std::uint32_t>(m_config.ControlRegister());
-    const auto data = nsw::intToByteVector(
-      i2c_val_32, nsw::NUM_BYTES_IN_WORD32, nsw::padtrigger::SCA_LITTLE_ENDIAN);
-
-    std::stringstream msg;
-    msg << " Writing  " << node << " reg " << address << " val " << std::hex
-        << m_config.ControlRegister() << " -> msg = ";
-    for (const auto& val : data) {
-      msg << std::hex << static_cast<unsigned>(val) << " " << std::dec;
-    }
-
-    writeRegister(address, node, data);
-
-    // FIXME 4 magic number, size of control register
-    const auto val =
-      nsw::byteVectorToWord32(readRegister(address, node, 4), nsw::padtrigger::SCA_LITTLE_ENDIAN);
-    ERS_INFO(m_opcserverIp << " " << m_scaAddress << " Readback ControlRegister: " << std::hex
-                           << val << std::dec);
-  } else {
-    ERS_INFO("Skipping configuration of control register " << m_opcserverIp << " " << m_scaAddress);
-  }
-}
-
-std::map<std::uint8_t, std::vector<std::uint32_t>> nsw::hw::PadTrigger::readConfiguration()
+void nsw::hw::PadTrigger::writeConfiguration() const
 {
   throw std::logic_error("Not implemented");
 }
 
-void nsw::hw::PadTrigger::writeRegister(const std::uint8_t regAddress,
-                                        const std::string& scaNode,
-                                        const std::vector<std::uint8_t>& data)
+std::map<std::uint8_t, std::vector<std::uint32_t>> nsw::hw::PadTrigger::readConfiguration() const
 {
-  const auto& opcconnection = OpcManager::getConnection(m_opcserverIp);
-  const auto addr = m_scaAddress + scaNode;
-
-  // address and data
-  const std::vector<std::uint8_t> address = {regAddress};
-
-  std::vector<std::uint8_t> payload = address;
-  payload.insert(payload.end(), data.begin(), data.end());
-
-  std::stringstream msg;
-  msg << " Writing  " << addr << " register " << std::hex << static_cast<std::size_t>(regAddress)
-      << std::dec << " -> msg = ";
-
-  for (const auto& val : payload) {
-    msg << std::hex << static_cast<unsigned>(val) << " " << std::dec;
-  }
-
-  ERS_DEBUG(3, msg.str());
-
-  DeviceInterface::SCA::sendI2cRaw(opcconnection, scaNode, payload.data(), payload.size());
+  throw std::logic_error("Not implemented");
 }
 
-std::vector<std::uint8_t> nsw::hw::PadTrigger::readRegister(const std::uint8_t regAddress,
-                                                            const std::string& scaNode,
-                                                            const std::size_t bytesToRead)
+void nsw::hw::PadTrigger::writeRegister([[maybe_unused]] const std::uint8_t regAddress,
+                                        [[maybe_unused]] const std::uint32_t value) const
 {
-  const auto& opcconnection = OpcManager::getConnection(m_opcserverIp);
-  const auto addr = m_scaAddress + scaNode;
+  throw std::logic_error("Not implemented");
+}
 
-  const std::vector<std::uint8_t> address = {regAddress};
-
-  return DeviceInterface::SCA::readI2cAtAddress(
-    opcconnection, m_scaAddress, address.data(), address.size(), bytesToRead);
+std::vector<std::uint8_t> nsw::hw::PadTrigger::readRegister(
+  [[maybe_unused]] const std::uint8_t regAddress) const
+{
+  throw std::logic_error("Not implemented");
 }
