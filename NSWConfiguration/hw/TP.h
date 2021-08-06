ee12a2449c3435bfbe83c2773342875cb9a457b4
Jared Sturdy <sturdy@cern.ch>
2021-08-06 17:14:59 +0200

    [TP] TP gut interface

--- NSWConfiguration/hw/TP.h
+++ NSWConfiguration/hw/TP.h
@@ -8,62 +8,67 @@
 
 #include "NSWConfiguration/TPConfig.h"
 
-namespace nsw {
-  namespace hw {
+namespace nsw::hw {
+  /**
+   * \brief Class representing a Trigger Processor
+   *
+   * Provides methods to read/write individual TP registers, as well
+   * as to write a complete configuration and read back all the
+   * registers contained in the configuration.
+   *
+   * Register mapping: TODO
+   * Documentation: TODO
+   */
+  class TP
+  {
+  public:
     /**
-     * \brief Class representing a Trigger Processor
-     *
-     * Provides methods to read/write individual TP registers, as well
-     * as to write a complete configuration and read back all the
-     * registers contained in the configuration.
+     * \brief Constrctor from a \ref TPConfig object
      */
-    class TP
-    {
-    public:
-      /**
-       * \brief Constrctor from a \ref TPConfig object
-       */
-      explicit TP(const TPConfig& config);
+    explicit TP(const TPConfig& config);
 
-      /**
-       * \brief Read the full TP address space
-       *
-       * \returns a map of address to register value
-       */
-      std::map<std::uint8_t, std::vector<std::uint32_t>> readConfiguration() const;
+    /**
+     * \brief Read the full TP address space
+     *
+     * \returns a map of address to register value
+     */
+    [[nodiscard]] std::map<std::uint8_t, std::vector<std::uint32_t>> readConfiguration() const;
 
-      /**
-       * \brief Write the full TP configuration
-       */
-      void writeConfiguration() const;
+    /**
+     * \brief Write the full TP configuration
+     */
+    void writeConfiguration() const;
 
-      /**
-       * \brief Write a value to a TP register address
-       *
-       * \param regAddress is the address of the register
-       * \param value is the value to be written
-       */
-      void writeRegister(std::uint8_t registerId, std::uint32_t value) const;
+    /**
+     * \brief Write a value to a TP register address
+     *
+     * \param regAddress is the address of the register
+     * \param value is the value to be written
+     */
+    void writeRegister([[maybe_unused]] std::uint8_t regAddress,
+                       [[maybe_unused]] std::uint32_t value) const;
 
-      /**
-       * \brief Read an individual TP register by its address
-       */
-      std::vector<std::uint8_t> readRegister(std::uint8_t registerId) const;
+    /**
+     * \brief Read an individual TP register by its address
+     *
+     * \param regAddress is the address of the register
+     */
+    [[nodiscard]] std::vector<std::uint8_t> readRegister(
+      [[maybe_unused]] std::uint8_t regAddress) const;
 
-      /**
-       * \brief Get the \ref TPConfig object associated with this TP object
-       *
-       * Both const and non-const overloads are provided
-       */
-      [[nodiscard]] TPConfig& getConfig() { return m_config; }
-      [[nodiscard]] const TPConfig& getConfig() const { return m_config; }  //!< \overload
+    /**
+     * \brief Get the \ref TPConfig object associated with this TP object
+     *
+     * Both const and non-const overloads are provided
+     */
+    [[nodiscard]] TPConfig& getConfig() { return m_config; }
+    [[nodiscard]] const TPConfig& getConfig() const { return m_config; }  //!< \overload
 
-    private:
-      TPConfig m_config;          //!< TPConfig object associated with this TP
-      std::string m_opcserverIp;  //!< address and port of Opc Server
-      std::string m_scaAddress;   //!< SCA address of TP item in Opc address space
-    };
-  }  // namespace hw
-}  // namespace nsw
+  private:
+    TPConfig m_config;          //!< TPConfig object associated with this TP
+    std::string m_opcserverIp;  //!< address and port of Opc Server
+    std::string m_scaAddress;   //!< SCA address of TP item in Opc address space
+  };
+}  // namespace nsw::hw
 
 #endif
