f4d29a428742dece140c1f17befd10ee1f5bc39a
Alexander Naip Tuna <alexander.tuna@cern.ch>
2022-03-30 10:30:01 +0200

    TPCarrier HW: fix one problem and minor improvements

--- src/hw/TPCarrier.cpp
+++ src/hw/TPCarrier.cpp
@@ -1,22 +1,26 @@
 #include "NSWConfiguration/hw/TPCarrier.h"
 #include "NSWConfiguration/hw/OpcManager.h"
 #include "NSWConfiguration/hw/SCAInterface.h"
+#include "NSWConfiguration/hw/SCAX.h"
 #include "NSWConfiguration/Constants.h"
 #include "NSWConfiguration/Utility.h"
 
 #include <stdexcept>
 
-nsw::hw::TPCarrier::TPCarrier(OpcManager& manager, const TPCarrierConfig& config) :
+nsw::hw::TPCarrier::TPCarrier(OpcManager& manager, const TPCarrierConfig& config):
   m_opcManager{manager},
   m_config(config),
   m_opcserverIp(config.getOpcServerIp()),
-  m_scaAddress(config.getAddress())
-{}
+  m_scaAddress(config.getAddress()),
+  m_busAddress(fmt::format("{}.I2C_0.bus0", m_scaAddress)),
+  m_name(fmt::format("{}/{}", m_opcserverIp, m_scaAddress))
+{
+}
 
 void nsw::hw::TPCarrier::writeConfiguration() const
 {
   const auto& regs = nsw::carrier::REGS;
-  writeRegister(regs.at("RJOUT_SEL"), m_config.RJOutSel());
+  writeAndReadbackRegister(regs.at("RJOUT_SEL"), m_config.RJOutSel());
 }
 
 std::map<std::uint32_t, std::uint32_t> nsw::hw::TPCarrier::readConfiguration() const
@@ -31,27 +35,17 @@ std::map<std::uint32_t, std::uint32_t> nsw::hw::TPCarrier::readConfiguration() c
 void nsw::hw::TPCarrier::writeRegister(const std::uint32_t regAddress,
                                        const std::uint32_t value) const
 {
-  ERS_LOG("Writing address, message =  " << static_cast<uint32_t>(regAddress) << ", " << value << " to "
-          << m_opcserverIp << "/" << m_opcserverIp);
-  const auto addr = nsw::intToByteVector(regAddress, nsw::NUM_BYTES_IN_WORD32, nsw::scax::SCAX_LITTLE_ENDIAN);
-  const auto data = nsw::intToByteVector(value,      nsw::NUM_BYTES_IN_WORD32, nsw::scax::SCAX_LITTLE_ENDIAN);
-
-  std::vector<uint8_t> payload;
-  payload.reserve(addr.size() + data.size());
-  payload.insert(std::end(payload), std::begin(addr), std::end(addr));
-  payload.insert(std::end(payload), std::begin(data), std::end(data));
-
-  const auto& opcConnection = m_opcManager.get().getConnection(m_opcserverIp, m_scaAddress);
-  nsw::hw::SCA::sendI2cRaw(opcConnection, m_scaAddress, payload.data(), payload.size());
+  nsw::hw::SCAX::writeRegister(getConnection(), m_busAddress, regAddress, value);
 }
 
 std::uint32_t nsw::hw::TPCarrier::readRegister(const std::uint32_t regAddress) const
 {
-  const auto addr = nsw::intToByteVector(regAddress, nsw::NUM_BYTES_IN_WORD32, nsw::scax::SCAX_LITTLE_ENDIAN);
+  return nsw::hw::SCAX::readRegister(getConnection(), m_busAddress, regAddress);
+}
 
-  const auto& opcConnection = m_opcManager.get().getConnection(m_opcserverIp, m_scaAddress);
-  const auto data = nsw::hw::SCA::readI2cAtAddress(opcConnection, m_scaAddress,
-                                                   addr.data(), addr.size(), nsw::NUM_BYTES_IN_WORD32);
-
-  return nsw::byteVectorToWord32(data, nsw::scax::SCAX_LITTLE_ENDIAN);
+void nsw::hw::TPCarrier::writeAndReadbackRegister(const std::uint32_t regAddress,
+                                                  const std::uint32_t value) const
+{
+  ERS_LOG(fmt::format("{}: writing to address 0x{:02x} with message 0x{:08x}", m_name, regAddress, value));
+  nsw::hw::SCAX::writeAndReadbackRegister(getConnection(), m_busAddress, regAddress, value);
 }
