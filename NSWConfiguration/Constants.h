028109ace0206f42ae64140f4bf911f8c14bc4aa
alexandertuna <alexander.tuna@gmail.com>
2022-07-18 22:41:45 +0200

    Pad trigger deskew: promote magic number to constant, and change from 1 to 2

--- NSWConfiguration/Constants.h
+++ NSWConfiguration/Constants.h
@@ -344,6 +344,7 @@ namespace nsw {
     constexpr bool SCA_LITTLE_ENDIAN = false;
     constexpr std::uint32_t NUM_INPUT_DELAYS       = 16;
     constexpr std::uint32_t NUM_INPUT_DELAYS_PER_BC = 6;
+    constexpr std::uint32_t NUM_INPUT_DELAYS_MARGIN = 2;
     constexpr std::uint32_t NUM_BITS_PER_PFEB_DELAY = 4;
     constexpr std::uint32_t NUM_PFEBS              = 24;
     constexpr std::uint32_t NUM_QUADS              = 6;
