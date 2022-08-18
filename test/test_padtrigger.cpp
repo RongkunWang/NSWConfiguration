4c9c8c21199f7dec36241ab654a2271046eaa6fd
Alexander Naip Tuna <alexander.tuna@cern.ch>
2022-08-18 15:01:43 +0200

    Another pad trigger deskew unit test

--- test/test_padtrigger.cpp
+++ test/test_padtrigger.cpp
@@ -4,6 +4,7 @@
 
 #include "NSWConfiguration/ConfigReader.h"
 #include "NSWConfiguration/hw/DeviceManager.h"
+constexpr std::size_t ONE{1};
 
 BOOST_AUTO_TEST_CASE(PadTrigger_checkPFEBBCIDs) {
   nsw::hw::DeviceManager deviceManager;
@@ -11,7 +12,7 @@ BOOST_AUTO_TEST_CASE(PadTrigger_checkPFEBBCIDs) {
   for (const auto& name : reader.getAllElementNames()) {
     deviceManager.add(reader.readConfig(name));
   }
-  BOOST_TEST(deviceManager.getPadTriggers().size() == 1);
+  BOOST_TEST(deviceManager.getPadTriggers().size() == ONE);
   const auto empty = BcidVector{};
   const auto zeros = BcidVector{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
   const auto fives = BcidVector{5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5};
@@ -28,3 +29,23 @@ BOOST_AUTO_TEST_CASE(PadTrigger_checkPFEBBCIDs) {
   }
 }
 
+BOOST_AUTO_TEST_CASE(PadTrigger_getViableBcids) {
+  nsw::hw::DeviceManager deviceManager;
+  nsw::ConfigReader reader("json://test_padtrigger.json");
+  for (const auto& name : reader.getAllElementNames()) {
+    deviceManager.add(reader.readConfig(name));
+  }
+  BOOST_TEST(deviceManager.getPadTriggers().size() == ONE);
+  const auto empty = BcidVector{};
+  const auto zeros = BcidVector{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+  const auto align = BcidVector{7,7,7,7,7,7,6,6,6,6,6,6,5,5,5,5,5,5,4,4,4,4,4,4};
+  const auto split = BcidVector{6,6,6,5,5,5,5,5,5,4,4,4,4,4,4,3,3,3,3,3,3,2,2,2};
+  const auto rollo = BcidVector{1,1,1,1,1,1,0,0,0,0,0,0,15,15,15,15,15,15,14,14,14,14,14,14};
+  for (const auto& dev: deviceManager.getPadTriggers()) {
+    BOOST_TEST((dev.getViableBcids(empty) == BcidVector{}));
+    BOOST_TEST((dev.getViableBcids(zeros) == BcidVector{}));
+    BOOST_TEST((dev.getViableBcids(align) == BcidVector{4, 5, 6, 7}));
+    BOOST_TEST((dev.getViableBcids(split) == BcidVector{3, 4, 5}));
+    BOOST_TEST((dev.getViableBcids(rollo) == BcidVector{0, 1, 14, 15}));
+  }
+}
