6823f855c3cd78e2833055fa5e9031282b20de64
Rongkun Wang <rongkun.wang@cern.ch>
2023-08-11 16:32:35 +0200

    move new register to last

--- src/hw/MMTP.cpp
+++ src/hw/MMTP.cpp
@@ -27,7 +27,6 @@ void nsw::hw::MMTP::writeConfiguration(bool doAlignArtGbtx) const
     {nsw::mmtp::REG_FIBER_BC_OFFSET,          FiberBCOffset()},
     {nsw::mmtp::REG_INPUT_PHASE,              GlobalInputPhase()},
     {nsw::mmtp::REG_INPUT_PHASEOFFSET,        GlobalInputOffset()},
-    {nsw::mmtp::REG_INPUT_PHASEL1OFFSET,      m_config.get<std::uint32_t>("gbtL1ddPhaseOffset", 0)},
     {nsw::mmtp::REG_SELFTRIGGER_DELAY,        SelfTriggerDelay()},
     {nsw::mmtp::REG_VMM_MASK_HOT_THRESH,      VmmMaskHotThresh()},
     {nsw::mmtp::REG_VMM_MASK_HOT_THRESH_HYST, VmmMaskHotThreshHyst()},
@@ -39,6 +38,7 @@ void nsw::hw::MMTP::writeConfiguration(bool doAlignArtGbtx) const
     {nsw::mmtp::REG_LAT_TX_IDLE_STATE,        0},
     {nsw::mmtp::REG_LAT_TX_BCID_OFFSET,       m_config.get<std::uint32_t>("latTxBcidOffset", 100)},
     {nsw::mmtp::REG_FIBER_REMAP_SEL,          fiberRemapSel()},
+    {nsw::mmtp::REG_INPUT_PHASEL1OFFSET,      m_config.get<std::uint32_t>("gbtL1ddPhaseOffset", 0)},
   };
 
   const auto skippedReg = SkipRegisters();
