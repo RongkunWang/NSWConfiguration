f53b33090267a28988740fb84137b5d21ae24134
Rongkun Wang <rongkun.wang@cern.ch>
2023-08-21 19:20:11 +0200

    fix

--- src/hw/MMTP.cpp
+++ src/hw/MMTP.cpp
@@ -38,7 +38,7 @@ void nsw::hw::MMTP::writeConfiguration(bool doAlignArtGbtx) const
     {nsw::mmtp::REG_LAT_TX_IDLE_STATE,        0},
     {nsw::mmtp::REG_LAT_TX_BCID_OFFSET,       m_config.get<std::uint32_t>("latTxBcidOffset", 100)},
     {nsw::mmtp::REG_FIBER_REMAP_SEL,          fiberRemapSel()},
-    {nsw::mmtp::REG_INPUT_PHASEL1OFFSET,      m_config.get<std::uint32_t>("gbtL1ddPhaseOffset", 0)},
+    {nsw::mmtp::REG_INPUT_PHASEL1DDCOFFSET,      m_config.get<std::uint32_t>("gbtL1ddPhaseOffset", 0)},
   };
 
   const auto skippedReg = SkipRegisters();
